2ec63730da412a9aae6c4875a383dd35e2d45c6b
evanbriscoe13 <evanbriscoe13@vt.edu>
2021-10-25 18:54:32 -0400

    Addition of for loop to identify questions that need response values changed along with file of those questions so that we don't need to query again.

--- 2017_2018_Join.R
+++ 2017_2018_Join.R
@@ -54,24 +54,64 @@ df_full <- merge(x = df_full, y = demo_17_18, by = "SEQN", all = TRUE)
 
 col_headers <- colnames(df_full)
 
-
+# ----------- Clean Header names --------------
 for (i in seq_along(col_headers)){
   # To handle column names ending in ".x" or ".y"
   if (substring(col_headers[i], nchar(col_headers[i])-1, nchar(col_headers[i])-1) == '.'){
-        
-        col_headers[i] <- substring(col_headers[i], 1, nchar(col_headers[i])-2)
-        
+    
+    col_headers[i] <- substring(col_headers[i], 1, nchar(col_headers[i])-2)
+    
   }
   # Set last character to lowercase if is a letter and letter before is number
   #strtoi is string to integer
-  if (is.na ( strtoi(  substring( col_headers[i], nchar( col_headers[i]) ) ) )  
-      && !is.na( strtoi( substring( col_headers[i], nchar( col_headers[i]) -1, nchar(col_headers[i]) -1) ) ) ){
-        col_headers[i] <- paste(substring(col_headers[i], 1, nchar(col_headers[i])-1),
-                          tolower(substring(col_headers[i], nchar(col_headers[i]))), sep = "")
+  # if (is.na ( strtoi(  substring( col_headers[i], nchar( col_headers[i]) ) ) )  
+  #     && !is.na( strtoi( substring( col_headers[i], nchar( col_headers[i]) -1, nchar(col_headers[i]) -1) ) ) ){
+  #       col_headers[i] <- paste(substring(col_headers[i], 1, nchar(col_headers[i])-1),
+  #                         tolower(substring(col_headers[i], nchar(col_headers[i]))), sep = "")
+  #}
+}
+#------------ Create translation function -----------
+translate <- function(cols){
+  table_names <- c("DIQ_J", "HDL_J","TRIGLY_J", "TCHOL_J", "GLU_J", "INS_J", "CBQPFC_J", "HSQ_J", "DBQ_J",  
+                   "MCQ_J", "PAQ_J", "PFQ_J", "WHQ_J", "CBQ_J", 
+                   "CBQPFA_J", "MCQ_J", "BMX_J", "BPQ_J", "DEMO_J")
+  trans_table <- list()
+  for (i in seq_along(table_names)){
+    if(length(nhanesTranslate(table_names[i], colnames = cols)) > 0){
+      return((nhanesTranslate(table_names[i], colnames = cols)))
+    }
   }
+  return (0)
+  
 }
 
 
 colnames(df_full) <- col_headers
+
+
+#frame <- (nhanesTranslate("DIQ_J", colnames = c("DIQ010","DID040")))
+#data.frame(frame[1])[,2]
+#names(frame)[1]
+
+response_values = list()
+a <- translate("MCQ371A")
+#question_var <- list()
+for (i in 501:length(colnames(df_full))) {
+  if(colnames(df_full[i])=='SEQN'){
+    i = i+1
+  }
+  tran <- translate(colnames(df_full[i]))
+  id <- names(tran)[1]
+  
+  responses <- tryCatch(data.frame(tran[1])[,2], error = function(e) {NULL})
+  if (('Yes' %in% responses && !('Range of Values' %in% responses)) ||
+      ('No' %in% responses && !('Range of Values' %in% responses))){
+    #question_var <- append(question_var,id)
+    response_values <- append(response_values,tran)
+  }
+}
+
+#save(response_values, file = "ResponseCleaningIdentifiers.RData")
+
 
 colnames(df_full)
