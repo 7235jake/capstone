4c324411c6732fb33307bf6bde461e5e422c2f9d
Lauren Bradley <lbrad543@vt.edu>
2021-10-31 21:52:25 -0400

    2009 / 2010 data cleaning!

--- 2009_2010_Join.R
+++ 2009_2010_Join.R
@@ -1,6 +1,21 @@
 library(nhanesA)
 library(tibble)
-library(dplyr)
+
+#------------ Create translation function -----------
+translate <- function(cols){
+  table_names <- c("DIQ_F", "HDL_F","TRIGLY_F", "TCHOL_F", "GLU_F", 
+                   "CBQ_F", "CBQPFA_F", "CBQPFC_F", "HSQ_I", "DBQ_I",  
+                   "MCQ_I", "PAQ_I", "PFQ_I", "WHQ_I", 
+                   "BMX_I", "BPQ_I", "DEMO_I")
+  trans_table <- list()
+  for (i in seq_along(table_names)){
+    if(length(nhanesTranslate(table_names[i], colnames = cols)) > 0){
+      return((nhanesTranslate(table_names[i], colnames = cols)))
+    }
+  }
+  return (0)
+  
+}
 
 # Diabetes
 dia_09_10 <- nhanes("DIQ_F")
@@ -28,6 +43,88 @@ bpq_09_10 <- nhanes("BPQ_F")
 
 # Demographic
 demo_09_10 <- nhanes("DEMO_F")
+
+# ------------------- Join all together -------------------------------
+df_full <- merge(x = dia_09_10, y = hdl_09_10, by = "SEQN", all = TRUE)
+df_full <- merge(x = df_full, y = tri_09_10, by = "SEQN", all = TRUE)
+df_full <- merge(x = df_full, y = chol_09_10, by = "SEQN", all = TRUE)
+df_full <- merge(x = df_full, y = glu_09_10, by = "SEQN", all = TRUE)
+df_full <- merge(x = df_full, y = cbq_09_10, by = "SEQN", all = TRUE)
+df_full <- merge(x = df_full, y = cbq_adult_09_10, by = "SEQN", all = TRUE)
+df_full <- merge(x = df_full, y = cbq_child_09_10, by = "SEQN", all = TRUE)
+df_full <- merge(x = df_full, y = dbq_09_10, by = "SEQN", all = TRUE)
+df_full <- merge(x = df_full, y = mcq_09_10, by = "SEQN", all = TRUE)
+df_full <- merge(x = df_full, y = paq_09_10, by = "SEQN", all = TRUE)
+df_full <- merge(x = df_full, y = pfq_09_10, by = "SEQN", all = TRUE)
+df_full <- merge(x = df_full, y = whq_09_10, by = "SEQN", all = TRUE)
+df_full <- merge(x = df_full, y = bmx_09_10, by = "SEQN", all = TRUE)
+df_full <- merge(x = df_full, y = bpq_09_10, by = "SEQN", all = TRUE)
+df_full <- merge(x = df_full, y = demo_09_10, by = "SEQN", all = TRUE)
+
+
+
+
+col_headers <- colnames(df_full)
+
+# ----------- Clean Header names --------------
+for (i in seq_along(col_headers)){
+  # To handle column names ending in ".x" or ".y"
+  if (substring(col_headers[i], nchar(col_headers[i])-1, nchar(col_headers[i])-1) == '.'){
+    
+    col_headers[i] <- substring(col_headers[i], 1, nchar(col_headers[i])-2)
+    
+  }
+}
+
+
+colnames(df_full) <- col_headers
+
+#------- Run script to determine which questions need responses mapped -------------------
+response_values = list()
+ 
+# Go through all columns in dataframe
+for (i in seq_along(colnames(df_full))) {
+#   #Translate call for SEQN returns an error so skip over
+   if(colnames(df_full[i])=='SEQN'){
+     i = i+1
+   }
+   # Grab translation of column
+   tran <- translate(colnames(df_full[i]))
+   
+   #tryCatch to avoid questions that will throw errors in translation method (ex: skip item)
+   responses <- tryCatch(data.frame(tran[1])[,2], error = function(e) {NULL})
+   if (('Yes' %in% responses && !('Range of Values' %in% responses)) ||
+       ('No' %in% responses && !('Range of Values' %in% responses))){
+     response_values <- append(response_values,tran)
+   }
+}
+
+save(response_values, file = "ResponseCleaningIdentifiers.RData")
+
+#---------- Change question responses ----------------
+# Goal: Change all responses not 1 (Yes) or NA to be 2 (No)
+
+#Store questions identifiers that need responses changed
+questions <- names(response_values)
+
+for (i in seq_along(questions)){
+  #Determine column number corresponding to question identifier
+  col_num <- which(colnames(df_full) == questions[i])
+  
+  for (row in seq_along(df_full[,col_num])){
+    
+    if( df_full[row,col_num] != 1 && df_full[row,col_num] != 2 && !is.na(df_full[row,col_num])){
+      df_full[row,col_num] <- 2
+    }
+    
+  }
+}
+
+#---------- Check if responses have changed ------------
+for (i in seq(questions)){
+  print(questions[i])
+  print(table(df_full[c(questions[i])]))
+}
 
 
 # -------------------Cleaning Diabetes Dataset-------------------------
