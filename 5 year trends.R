eb1735338b50a61e3a4fe77a35f9445a7b20b454
7235jake <jlavitt22@vt.edu>
2021-11-15 13:56:36 -0500

    5 year plots

--- 5 year trends.R
+++ 5 year trends.R
@@ -1,4 +1,6 @@
 library(tidyverse)
+library(ggplot2)
+library(ggalt)
 
 load("NHANESCleanFactors.RData")
 load("TranslationsFull.RData")
@@ -82,7 +84,16 @@ index <- c(1, 2, 3, 4, 5)
 
 for(i in 1:length(colnames(common_09_10)))
 {
-  df <- data.frame(index, avgs[,i])
-  ggplot(df)
-}
+  df <- data.frame("Year" = c("2009-2010", "2011-2012", "2013-2014", "2015-2016", "2017-2018"), "Averages" = avgs[,i])
+  df
+  gg <- ggplot(df, aes(x=Year, y=Averages, group=1)) +  geom_line() + geom_point(aes(size=Averages)) +
+    theme(axis.text.x = element_text(angle = 45, hjust=1)) +
+    labs(subtitle=colnames(common_09_10)[i], 
+         y="Average", x="Year", title="Average Vs Year")
+  
+  # Saving Plots
+  
+  filename = paste("C:/Users/owner/OneDrive/CMDA Capstone/capstone/ROV_avgs_5_year_plots/", colnames(common_09_10)[i],"-", "Avg_vs_Year.pdf", sep="")
+  ggsave(filename, gg)
 
+}
