7de0f48b933abe30f15f8c7c28b6c9ccfd8c1b19
7235jake <jlavitt22@vt.edu>
2021-11-23 12:36:11 -0500

    script

--- winsorize_tableones.R
+++ winsorize_tableones.R
@@ -2,25 +2,27 @@ library(tidyverse)
 library(DescTools)
 load("selected_df.RData")
 
-x <- selected_df
-
-cols <- colnames(x)
-
-for(i in 1:length(cols))
-{
-  if(!(is.factor(x[,i]))){
-    q <- quantile(x[,i], c(0.025, 0.975), na.rm = TRUE, type = 1)
-    for(j in 1:length(x[,i]))
-    {
-      if(!(is.na(x[j, i]))){
-        if(x[j, i] < q[1]){
-          x[j, i] <- q[1]
-        }
-        if(x[j, i] > q[2]){
-          x[j, i] <- q[2]
-        }
-      }
-    }
-  }
-}
+# x <- selected_df
+# 
+# cols <- colnames(x)
+# 
+# for(i in 1:length(cols))
+# {
+#   if(!(is.factor(x[,i]))){
+#     q <- quantile(x[,i], c(0.025, 0.975), na.rm = TRUE, type = 1)
+#     for(j in 1:length(x[,i]))
+#     {
+#       if(!(is.na(x[j, i]))){
+#         if(x[j, i] < q[1]){
+#           x[j, i] <- q[1]
+#         }
+#         if(x[j, i] > q[2]){
+#           x[j, i] <- q[2]
+#         }
+#       }
+#     }
+#   }
+# }
+# 
+# save(x, file = "selected_df.RData")
 
