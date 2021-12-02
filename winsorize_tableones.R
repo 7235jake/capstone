81cffbaa1e9e7770880cd72683837889096f2d06
7235jake <jlavitt22@vt.edu>
2021-12-02 16:11:15 -0500

    table one attempt 1

--- winsorize_tableones.R
+++ winsorize_tableones.R
@@ -46,19 +46,32 @@ winsorize <- function(df)
 #   }
 # }
 
-select <- winsorize(selected_df)
-df0910 <- winsorize(df_full2009)
-df1112 <- winsorize(df_11_12)
-df1314 <- winsorize(df_13_14)
-df1516 <- winsorize(df_15_16)
-df1718 <- winsorize(df_17_18)
+# # Running the Function and Saving
 
+# select <- winsorize(selected_df)
+# df0910 <- winsorize(df_full2009)
+# df1112 <- winsorize(df_11_12)
+# df1314 <- winsorize(df_13_14)
+# df1516 <- winsorize(df_15_16)
+# df1718 <- winsorize(df_17_18)
+# 
+# 
+# save(select, file = "selected_Winsorized_df.RData")
+# save(df0910, file = "NHANES_Winsorized_2009_2010.RData")
+# save(df1112, file = "NHANES_Winsorized_2011_2012.RData")
+# save(df1314, file = "NHANES_Winsorized_2013_2014.RData")
+# save(df1516, file = "NHANES_Winsorized_2015_2016.RData")
+# save(df1718, file = "NHANES_Winsorized_2017_2018.RData")
 
-save(select, file = "selected_Winsorized_df.RData")
-save(df0910, file = "NHANES_Winsorized_2009_2010.RData")
-save(df1112, file = "NHANES_Winsorized_2011_2012.RData")
-save(df1314, file = "NHANES_Winsorized_2013_2014.RData")
-save(df1516, file = "NHANES_Winsorized_2015_2016.RData")
-save(df1718, file = "NHANES_Winsorized_2017_2018.RData")
-
-
+# # Table Ones
+# install.packages("tableone")
+# library(tableone)
+# 
+# load("NHANES_Winsorized_2009_2010.RData")
+# load("NHANES_Winsorized_2011_2012.RData")
+# load("NHANES_Winsorized_2013_2014.RData")
+# load("NHANES_Winsorized_2015_2016.RData")
+# load("NHANES_Winsorized_2017_2018.RData")
+# 
+# vars <- c("LBXGLU", "WTINT2YR", "PAD675", "LBXTC", "BMXBMI")
+# CreateTableOne(vars = vars, strata = "DIQ010", data = df0910)
